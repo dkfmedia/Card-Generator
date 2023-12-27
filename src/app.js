bb4eb1bea987bef0ab8dda0ce635b1588652a69e
Dana M <147849932+dkfmedia@users.noreply.github.com>
2023-12-27 22:49:44 +0000

    project finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".card").classList.add(generateRandomIcons());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * 13);
+  return numbers[indexNumbers];
+};
+
+let generateRandomIcons = () => {
+  let icons = ["diamonds", "hearts", "spades", "clubs"];
+  let indexIcons = Math.floor(Math.random() * 4);
+  return icons[indexIcons];
+};
+
+const refreshButton = document.querySelector("#btn");
+
+const refreshPage = () => {
+  location.reload();
+};
+
+refreshButton.addEventListener("click", refreshPage);
